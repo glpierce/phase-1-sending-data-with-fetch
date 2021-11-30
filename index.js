f31eb24e987e82e32a4ba5e061ca572bedfab928
Grant Pierce <glpierce174@gmail.com>
2021-11-30 14:51:56 -0800

    commit tree ecddad68104fa0ef41c25681ec32f5c1f7c4fbc7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function createRequestObj(nameInput, emailInput) {
+    const formData = {name: nameInput, email: emailInput};
+    const requestObj = {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                                Accept: "application/json",
+                            },
+                            body: JSON.stringify(formData)
+                        };
+    return requestObj;
+}
+
+
+function submitData(nameInput, emailInput) {
+   return fetch("http://localhost:3000/users", createRequestObj(nameInput, emailInput))
+    .then((resp) => resp.json())
+    .then((data) => {
+        const id = document.createElement('p')
+        id.textContent = data.id
+        document.querySelector('body').append(id)
+    })
+    .catch((error) => {
+        const errorMessage = document.createElement('p')
+        errorMessage.textContent = error.message
+        document.querySelector('body').append(errorMessage)
+    })
+}
